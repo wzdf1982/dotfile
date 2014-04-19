162ecc80b2aeb5bfaa28cf882466f21a998389fa
Dingjun Zhou <dingjun.zhou@gmail.com>
2014-04-19 09:09:00 +0800

    add deploy

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+cp -fr .vim ../
+cp .bash_profile ../
+
+mkdir ../.janus
+cd ../.janus
+
+git clone https://github.com/vim-scripts/DeleteTrailingWhitespace
+git clone https://github.com/fholgado/minibufexpl.vim
+git clone https://github.com/Townk/vim-autoclose
+git clone https://github.com/thoughtbot/vim-rspec
